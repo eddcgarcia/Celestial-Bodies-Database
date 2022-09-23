c50a365df643919dadb219ef10832ddb0a6b8c99
eddcgarcia <58670498+eddcgarcia@users.noreply.github.com>
2022-09-23 12:05:21 -0700

    Update universe.py

--- universe.py
+++ universe.py
@@ -3,8 +3,12 @@ from sqlalchemy.orm import sessionmaker
 
 from galaxy import Galaxy
 from star import Star
+from planet import Planet
+from moon import Moon
+from asteroid import Asteroid
 from base import Base
 
+from random import seed, uniform, randrange
 
 engine = create_engine('postgresql://freecodecamp@localhost:5432/universe')
 Session = sessionmaker(bind=engine)
@@ -12,11 +16,33 @@ session = Session()
 
 Base.metadata.create_all(engine)
 
-galaxyA = Galaxy('galaxyA', 10, 10, 10, 'This is galaxyA', False, False)
-starA = Star('starA', 10, 10, 10, 'This is starA', False, False, galaxyA)
+seed(42)
 
-session.add(galaxyA)
-session.add(starA)
+for g in range(6):
+    g_name = f'galaxy_g{g}'
+    galaxy = Galaxy(g_name, randrange(1000), randrange(1000), randrange(10000), \
+        f'This is {g_name}', False, False)
+    session.add(galaxy) 
+    for a in range(6):
+        a_name = f'asteroid_g{g}_a{a}'
+        asteroid = Asteroid(a_name, randrange(1000), randrange(1000), randrange(10000), \
+            f'This is {a_name}', False, False, galaxy)  
+        session.add(asteroid)
+    for s in range(6):
+        s_name = f'star_g{g}_s{s}'
+        star = Star(s_name, randrange(1000), randrange(1000), randrange(10000), \
+            f'This is {s_name}', False, False, galaxy)  
+        session.add(star)
+        for p in range(12):
+            p_name = f'planet_g{g}_s{s}_p{p}'
+            planet = Planet(p_name, randrange(1000), randrange(1000), randrange(10000), \
+                f'This is {p_name}', False, False, star)  
+            session.add(planet)
+            for m in range(20):
+                m_name = f'moon_g{g}_s{s}_p{p}_m{m}'
+                moon = Moon(m_name, randrange(1000), randrange(1000), randrange(10000), \
+                    f'This is {m_name}', False, False, planet)  
+                session.add(moon)
 
 session.commit()
 session.close()
